6a7431728f380a96cbbaa6073698b9c009b7c21d
Mitch Lichtenberg <mpl@apple.com>
2020-07-15 23:50:40 -0700

    change copyright for tcpacket.h, it is not private.

--- Firmware/communication/tcpacket.h
+++ Firmware/communication/tcpacket.h
@@ -8,16 +8,8 @@
     *  
     *********************************************************************  
     *
-    *  Copyright (C) 2020  Apple, Inc. All rights reserved.
+    *  Author: Mitch Lichtenberg (mitch@lightmountain.org)
     *
-    *  Author: Mitch Lichtenberg (mpl@apple.com)
-    *  
-    *  This document is the property of Apple Inc.
-    *  It is considered confidential and proprietary.
-    *
-    *  This document may not be reproduced or transmitted in any form,
-    *  in whole or in part, without the express written permission of
-    *  Apple Inc.
     ********************************************************************* */
 
 #ifndef _TCPACKET_H_
